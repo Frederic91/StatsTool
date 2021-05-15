c34692865fca98d482df0f6499a516f6c52ac6b5
Frédéric _ <frederic_91@orange.fr>
2021-05-15 23:12:33 +0200

    setup.sh correction

--- .ipynb_checkpoints/setup-checkpoint.sh
+++ .ipynb_checkpoints/setup-checkpoint.sh
@@ -1,6 +1,13 @@
-mkdir -p ~/.streamlit
-echo "[server]
-headless=true
-port=$PORT
-enableCORS=false
-" > /.streamlit/config.toml
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"frederic_91@orange.fr\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
